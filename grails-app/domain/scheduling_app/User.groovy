8b46e43eb296e586b507b79714c91fe5a985b16e
Stephen DiCerce <sdicerce@oswego.edu>
2017-07-02 22:18:16 -0400

    added user domain class

---
+++ grails-app/domain/scheduling_app/User.groovy
@@ -0,0 +1,27 @@
+package scheduling_app
+
+class User {
+
+    String userName
+    String firstName
+    String lastName
+    String email
+    String imageUrl
+
+    static hasOne = [authToken: AuthToken]
+    static hasMany = [event: Event]
+
+    static mapping = {
+        table "users"
+        version false
+    }
+
+    static constraints = {
+        userName unique: true
+        firstName nullable: true
+        lastName nullable: true
+        email unique: true
+        authToken nullable: true
+        imageUrl nullable: true, blank: false
+    }
+}
